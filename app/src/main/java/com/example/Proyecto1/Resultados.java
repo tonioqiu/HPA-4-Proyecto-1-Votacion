aed8910cd4c39c5cb18aa85e1e0e453d2a07028b
tonioqiu <tonioqiu.dev@gmail.com>
2022-09-22 11:04:16 -0500

    Added Progress Bar To Result

--- app/src/main/java/com/example/Proyecto1/Resultados.java
+++ app/src/main/java/com/example/Proyecto1/Resultados.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
@@ -13,16 +15,19 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 
 public class Resultados extends AppCompatActivity {
 
-    TextView txtVotosOmar, txtVotosVivian, txtVotosMartin;
+    TextView txtVotosOmar, txtVotosVivian, txtVotosMartin, txtpercent1, txtpercent2,txtpercent3;
     Button buttonR;
-    int voto1, voto2, voto3;
+    int voto1, voto2, voto3, votoTotal=0;
+    double voto1percent, voto2percent, voto3percent;
     String filename = "";
     String filepath = "";
-
+    ProgressBar bar1, bar2, bar3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +40,12 @@ public class Resultados extends AppCompatActivity {
         txtVotosOmar = findViewById(R.id.txtVotosOmar);
         txtVotosMartin = findViewById(R.id.txtVotosMartin);
         txtVotosVivian = findViewById(R.id.txtVotosVivian);
+        txtpercent1 = findViewById(R.id.txtpercent1);
+        txtpercent2 = findViewById(R.id.txtpercent2);
+        txtpercent3 = findViewById(R.id.txtpercent3);
+        bar1 = findViewById(R.id.barVoto1);
+        bar2 = findViewById(R.id.barVoto2);
+        bar3 = findViewById(R.id.barVoto3);
 
         //asignar boton
         buttonR = findViewById(R.id.btnRegresar);
@@ -51,11 +62,34 @@ public class Resultados extends AppCompatActivity {
             else if (Integer.parseInt(votoArray[i]) == 3)
                 voto3++;
         }
+        //Calculo de total de votos
+        votoTotal= voto1+voto2+voto3;
+        //Calculo del porcentaje de votos de cada candidato
+        voto1percent = voto1*100.0/votoTotal;
+        voto2percent = voto2*100.0/votoTotal;
+        voto3percent = voto3*100.0/votoTotal;
+
+
+        //impresion de porcentaje en texto
+        NumberFormat formatter = new DecimalFormat("#0.00");
+        String p1 = formatter.format(voto1percent) + "%";
+        String p2 = formatter.format(voto2percent) + "%";
+        String p3 = formatter.format(voto3percent) + "%";
+
+        txtpercent1.setText(p1);
+        txtpercent2.setText(p2);
+        txtpercent3.setText(p3);
+
+        //impresion de porcentaje en progressbar
+        bar1.setProgress((int)voto1percent);
+        bar2.setProgress((int)voto2percent);
+        bar3.setProgress((int)voto3percent);
 
         //Camibio del texto en el textView
         String a=Integer.toString(voto1);
         String b=Integer.toString(voto2);
         String c=Integer.toString(voto3);
+
         txtVotosOmar.setText(a);
         txtVotosMartin.setText(b);
         txtVotosVivian.setText(c);
